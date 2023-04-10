e2bd909a40e22419c0b75c7100f19c06013b12ad
Bimalsha Hasanthi <90233621+BimalshaHasanthi@users.noreply.github.com>
2023-04-11 00:48:07 +0530

    Added settings page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Market from './pages/market/Market'
+import Settings from './pages/settings/Settings'
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/settings" element={<Settings />} />
       <Route path="/market" element={<Market />} />
     </Routes>
   )

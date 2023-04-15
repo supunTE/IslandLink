98fb62ecd4777a21732e931bf9d0e173c1aef734
Bimalsha Hasanthi <90233621+BimalshaHasanthi@users.noreply.github.com>
2023-04-15 10:46:37 +0530

    Updated location page

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Settings from './pages/settings/Settings'
 import Community from './pages/community/Community'
+import Location from './pages/location/Location'
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/community" element={<Community />} />
+      <Route path="/location" element={<Location />} />
     </Routes>
   )
 }
